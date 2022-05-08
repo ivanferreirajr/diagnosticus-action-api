05981cb5335411814981c731e3c76d5c99b9831a
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-05-08 20:09:26 -0300

    feat: add sequelize in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './modules/users/entities/user.entity';
+import { UsersModule } from './modules/users/users.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME_DEVELOPMENT,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      models: [User],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
