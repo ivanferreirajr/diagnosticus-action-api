93c755dadf369d6b69e64aa7dab0908316a3c9df
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-05-08 20:22:08 -0300

    feat(swagger): add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Diagnosticus Action - API Rest')
+    .setDescription('if this is not the prettiest API ever, I am a fridge')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
